76fd00f13770458de5a0fa3e0749de6a57c0f551
Pekka Harjamäki <spamtrap_github@mcfish.org>
2022-12-07 09:53:53 +0200

    Added more tests

--- tests/unitTest1.js
+++ tests/unitTest1.js
@@ -1,9 +1,29 @@
 $("#select1").tinyselect();
 
 QUnit.test("Structure creation", ( assert ) => {
-  const ts1 = $("#select1_ts");
+  // This element should exist if plugin works at all
+  const el1 = document.querySelector("#select1_ts");
+  assert.ok( el1 , "Structure found" );
 
-  assert.expect(1);
-  assert.equal( 1 , ts1.length , "Structure found" );
+  // Try to find and click menu
+  const el2 = document.querySelector("div.selectbox");
+  assert.equal( "selectbox", el2.className, "Select is closed by default" );
+  el2.click();
+  assert.equal( "selectbox open", el2.className, "Select opened when clicked" );
+
+  // Try to find searchbox
+  const el3 = document.querySelector("input.searchbox");
+  const el4 = document.querySelector("ul.itemcontainer");
+  assert.ok( el3 , "Search input field found" );
+  assert.equal(el4.children.length, 8, "All children present in optionlist");
+
+  el3.value = "option 1";
+  el3.dispatchEvent(new Event("keyup"));
+  assert.equal(el4.children.length, 0, "Search test1");
+
+  el3.value = "option a";
+  el3.dispatchEvent(new Event("keyup"));
+  assert.equal(el4.children.length, 1, "Search test2");
+
 });
 
