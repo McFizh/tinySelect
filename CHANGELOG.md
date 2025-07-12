64fefc4efa606084ad1767cd8e5924345dfb4e93
Pekka Harjamäki <spamtrap_github@mcfish.org>
2025-07-12 22:24:56 +0300

    Update dev dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,36 +1,43 @@
-## 2.0.1 (1.8.2024)
+### Changelog
 
-- Added 'setDataUrl' command for the plugin
-- Updated (dev)dependencies
-- Dropped jquery 3.5 from tests and replaced it with jquery 3.7
-- Updated pipeline to use latest node version / actions
+- **3.0.0** - 14.7.2025
 
-## 2.0.0 (30.11.2022)
+  - Dropped support for jquery 2
+  - Updated eslint from 8 to 9 + dev dependency updates
 
-Code cleanup:
+- **2.0.1** - 1.8.2024
 
-- Dropped support for jquery 1
-- All var definitions have been change to either const or let
-- Added qunit test for latest jquery 3.6 and previous 3.5
-- Updated grunt & qunit packages
-- Replaced jshint with eslint
-- Migrated from travis to github actions
-- Updated node version used in automated testing from 8 to 14
-- Added http-server page so that demo can be easily started
-- Moved source files to src directory
+  - Added 'setDataUrl' command for the plugin
+  - Updated (dev)dependencies
+  - Dropped jquery 3.5 from tests and replaced it with jquery 3.7
+  - Updated pipeline to use latest node version / actions
 
-## 1.0.6 (16.7.2018)
+- **2.0.0** - 30.11.2022
 
-Code cleanup:
+  - Code cleanup:
 
-- Updated dev package versions
-- Recompiled with newer js uglify (this in turn reduced filesize by 7 bytes.. Yay)
-- Replaces tabs from source
+  - Dropped support for jquery 1
+  - All var definitions have been change to either const or let
+  - Added qunit test for latest jquery 3.6 and previous 3.5
+  - Updated grunt & qunit packages
+  - Replaced jshint with eslint
+  - Migrated from travis to github actions
+  - Updated node version used in automated testing from 8 to 14
+  - Added http-server page so that demo can be easily started
+  - Moved source files to src directory
 
-## 1.0.5 (9.7.2016)
+- **1.0.6** - 16.7.2018
 
-Bug fixes:
+  - Code cleanup:
 
-- Text filtering can now be set as case sensitive or insensitive
-- tinySelect main div is now given id, if original select has id. The new id is original + \_ts
-- Updated qunit from 1.x to 2.x and added some basic tests against jquery 1.x , 2.x and 3.x
+  - Updated dev package versions
+  - Recompiled with newer js uglify (this in turn reduced filesize by 7 bytes.. Yay)
+  - Replaces tabs from source
+
+- **1.0.5** - 9.7.2016
+
+  - Bug fixes:
+
+  - Text filtering can now be set as case sensitive or insensitive
+  - tinySelect main div is now given id, if original select has id. The new id is original + \_ts
+  - Updated qunit from 1.x to 2.x and added some basic tests against jquery 1.x , 2.x and 3.x
--- src/tinyselect.js
+++ src/tinyselect.js
@@ -3,11 +3,10 @@
  *
  * Licensed under MIT license.
  *
- * @version 2.0.1
+ * @version 3.0.0
  * @author Pekka Harjamäki
  */
-// eslint-disable-next-line no-extra-semi
-;(function($) {
+;(function ($) {
 
   "use strict";
 
@@ -15,8 +14,8 @@
     /* ******************************************************************* *
      * Class initializers
      * ******************************************************************* */
-    init: function($el, options) {
-      $el.data("tinySelectObj",this);
+    init: function ($el, options) {
+      $el.data("tinySelectObj", this);
 
       this.config = $.extend({
         showSearch: true,
@@ -26,7 +25,7 @@
 
         dataUrl: null,
         dataParser: null
-      },options);
+      }, options);
 
       this.state = {
         container: null,
@@ -51,20 +50,20 @@
 
     },
 
-    createSelect: function($el) {
+    createSelect: function ($el) {
       // Create container for select, search and options
       this.state.container = $("<div></div>").
         addClass("tinyselect").
         css({ width: $el.css("width") });
 
       const t_id = $el.attr("id");
-      if( t_id && t_id.length > 0 )
-        this.state.container.attr("id",t_id+"_ts");
+      if (t_id && t_id.length > 0)
+        this.state.container.attr("id", t_id + "_ts");
 
       // Create the select element
       this.state.selectBox = $("<div></div>").
         addClass("selectbox").
-        on("click", { self:this }, this.onSelectBoxClicked );
+        on("click", { self: this }, this.onSelectBoxClicked);
 
       this.state.container.append(this.state.selectBox);
 
@@ -76,7 +75,7 @@
       this.state.container.append(this.state.dropdown);
 
       // Add search as first element
-      if(this.config.showSearch)
+      if (this.config.showSearch)
         this.createSearch(this.state.dropdown);
 
       // Create ul to hold items
@@ -91,78 +90,75 @@
       this.state.$el = $el;
 
       // Hide select content when clicked elsewhere in the document
-      $(document).on("click", {self: this}, this.onDocumentClicked );
+      $(document).on("click", { self: this }, this.onDocumentClicked);
     },
 
-    createItems: function() {
+    createItems: function () {
       // Remove all
       this.state.itemContainer.empty();
 
       //
-      for(const opt of this.state.filteredItemData)
-      {
+      for (const opt of this.state.filteredItemData) {
         const newLi = $("<li></li>").text(opt.text).addClass("item").attr("data-value", opt.val);
 
-        if( opt.val === this.state.selectedValue )
-        {
-          this.state.selectBox.html( opt.text );
+        if (opt.val === this.state.selectedValue) {
+          this.state.selectBox.html(opt.text);
           newLi.addClass("selected");
         }
 
-        newLi.on("click", { self:this }, this.onSelectLiClicked );
+        newLi.on("click", { self: this }, this.onSelectLiClicked);
 
         this.state.itemContainer.append(newLi);
       }
     },
 
-    createSearch: function() {
+    createSearch: function () {
       this.state.searchContainer = $("<div></div>").
         addClass("searchcontainer");
       this.state.searchBox = $("<input type='text'></input>").
         addClass("searchbox").
-        on("click",function(e) { e.stopPropagation(); }).
-        on("keyup",{ self: this }, this.onSearchKeyPress);
+        on("click", function (e) { e.stopPropagation(); }).
+        on("keyup", { self: this }, this.onSearchKeyPress);
 
       this.state.searchContainer.append($("<span class='searchicon'></span>"));
       this.state.searchContainer.append(this.state.searchBox);
       this.state.dropdown.append(this.state.searchContainer);
     },
 
-    readSelect: function($el) {
+    readSelect: function ($el) {
       const self = this;
 
-      $el.find("option").each(function(){
+      $el.find("option").each(function () {
         const opt = $(this);
-        self.state.originalItemData.push({ val: opt.val() , text: opt.text() });
+        self.state.originalItemData.push({ val: opt.val(), text: opt.text() });
       });
 
       this.state.filteredItemData = this.state.originalItemData;
       this.state.selectedValue = $el.val();
     },
 
-    loadData: function(url) {
+    loadData: function (url) {
       const self = this;
       self.setAjaxIndicator(false);
       $.ajax({ url, dataType: "json", type: "GET" })
-        .done( function(data) { self.onAjaxLoadSuccess(self, data); } )
-        .fail( function() { self.onAjaxLoadError(self); } );
+        .done(function (data) { self.onAjaxLoadSuccess(self, data); })
+        .fail(function () { self.onAjaxLoadError(self); });
     },
 
-    setAjaxIndicator: function(failure) {
+    setAjaxIndicator: function (failure) {
       this.state.ajaxPending = true;
       this.state.itemContainer.empty();
 
-      if(this.state.searchContainer !== null)
+      if (this.state.searchContainer !== null)
         this.state.searchContainer.hide();
 
       const newLi = $("<li></li>");
-      if(!failure)
-      {
-        newLi.text( this.config.txtLoading ).
-          addClass( "loadindicator" );
+      if (!failure) {
+        newLi.text(this.config.txtLoading).
+          addClass("loadindicator");
       } else {
-        newLi.text( this.config.txtAjaxFailure ).
-          addClass( "loaderrorindicator" );
+        newLi.text(this.config.txtAjaxFailure).
+          addClass("loaderrorindicator");
       }
 
       this.state.itemContainer.append(newLi);
@@ -171,48 +167,46 @@
     /* ******************************************************************* *
      * Event handlers
      * ******************************************************************* */
-    onDocumentClicked: function(e) {
+    onDocumentClicked: function (e) {
       const self = e.data.self;
 
-      if( self.state.open )
+      if (self.state.open)
         self.onSelectBoxClicked(e);
     },
 
-    onSearchKeyPress: function(e) {
+    onSearchKeyPress: function (e) {
       const self = e.data.self;
       let sval = $(e.currentTarget).val();
 
       // Convert search string to lowercase, if using case insensitive search
-      if(!self.config.searchCaseSensitive)
+      if (!self.config.searchCaseSensitive)
         sval = sval.toLowerCase();
 
-      if(sval.length === 0)
-      {
+      if (sval.length === 0) {
         self.state.filteredItemData = self.state.originalItemData;
       } else {
-        self.state.filteredItemData = self.state.originalItemData.filter(function(item){
+        self.state.filteredItemData = self.state.originalItemData.filter(function (item) {
           // Case insensitive search
-          if(!self.config.searchCaseSensitive)
-            return item.text.toLowerCase().indexOf(sval) >= 0 ? true: false;
+          if (!self.config.searchCaseSensitive)
+            return item.text.toLowerCase().indexOf(sval) >= 0 ? true : false;
 
           // Case sensitive search
-          return item.text.indexOf(sval) >= 0 ? true: false;
+          return item.text.indexOf(sval) >= 0 ? true : false;
         });
       }
 
       self.createItems();
     },
 
-    onSelectBoxClicked: function(e) {
+    onSelectBoxClicked: function (e) {
       const self = e.data.self;
 
       // Do nothing, if currently animating
-      if(self.state.dropdown.is(":animated"))
+      if (self.state.dropdown.is(":animated"))
         return;
 
       // Close selectBox
-      if( self.state.open )
-      {
+      if (self.state.open) {
         self.state.open = false;
         self.state.selectBox.removeClass("open");
         self.state.dropdown.slideUp(100);
@@ -220,7 +214,7 @@
       }
 
       // Open selectbox
-      if(self.config.dataUrl !== null)
+      if (self.config.dataUrl !== null)
         self.loadData(self.config.dataUrl);
 
       self.state.open = true;
@@ -228,44 +222,44 @@
       self.state.dropdown.slideDown(100);
     },
 
-    onAjaxLoadSuccess: function(self, data) {
+    onAjaxLoadSuccess: function (self, data) {
       self.state.ajaxPending = false;
 
-      if(self.config.dataParser !== null )
+      if (self.config.dataParser !== null)
         data = self.config.dataParser(data, self.state.selectedValue);
 
       self.state.$el.empty();
-      data.forEach(function(v){
-        if(v.selected)
+      data.forEach(function (v) {
+        if (v.selected)
           self.state.selectedValue = v.val;
 
-        self.state.$el.append( $("<option></option>").text(v.text).val(v.val) );
+        self.state.$el.append($("<option></option>").text(v.text).val(v.val));
       });
 
-      self.state.$el.val( self.state.selectedValue );
+      self.state.$el.val(self.state.selectedValue);
       self.state.originalItemData = data;
       self.state.filteredItemData = data;
 
-      if(this.state.searchContainer !== null)
+      if (this.state.searchContainer !== null)
         this.state.searchContainer.show();
 
       self.createItems();
     },
 
-    onAjaxLoadError: function(self) {
+    onAjaxLoadError: function (self) {
       self.setAjaxIndicator(true);
     },
 
-    onSelectLiClicked: function(e) {
+    onSelectLiClicked: function (e) {
       const self = e.data.self;
       const item = $(e.currentTarget);
 
-      self.state.dropdown.find("li").each(function() {
+      self.state.dropdown.find("li").each(function () {
         $(this).removeClass("selected");
       });
 
       item.addClass("selected");
-      self.state.selectBox.html( item.text() );
+      self.state.selectBox.html(item.text());
 
       self.state.selectedValue = item.attr("data-value");
       self.state.$el.val(self.state.selectedValue);
@@ -275,7 +269,7 @@
     /* ******************************************************************* *
      * External callbacks
      * ******************************************************************* */
-    onChangeDataUrl: function(newUrl) {
+    onChangeDataUrl: function (newUrl) {
       this.config.dataUrl = newUrl;
     },
   };
@@ -283,21 +277,21 @@
   /* ******************************************************************* *
    * Plugin main
    * ******************************************************************* */
-  $.fn.tinyselect = function(options) {
-    if(options === undefined || typeof options === "object") {
-      return this.each( function() {
+  $.fn.tinyselect = function (options) {
+    if (options === undefined || typeof options === "object") {
+      return this.each(function () {
         const key = "plugin_tinySelect";
-        if(!$.data(this, key)) {
+        if (!$.data(this, key)) {
           const sel = Object.create(TinySelect);
-          sel.init( $(this) , options);
+          sel.init($(this), options);
           $.data(this, key, sel);
         }
       });
-    } else if(typeof options === "string" && options === "setDataUrl") {
+    } else if (typeof options === "string" && options === "setDataUrl") {
       const args = arguments;
-      return this.each( function() {
+      return this.each(function () {
         const instance = $.data(this, "plugin_tinySelect");
-        if(instance && arguments[1])
+        if (instance && arguments[1])
           instance.onChangeDataUrl(args[1]);
       });
     }
